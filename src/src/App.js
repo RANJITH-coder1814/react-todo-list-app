740180dda6ba5b39f7d96cb519bcfa932ab91302
Ranjith.HL <ranjithranjith2747@gmail.com>
2026-03-01 23:19:16 +0530

    Create App.js

---
+++ src/src/App.js
@@ -0,0 +1,55 @@
+import React, { useState, useEffect } from "react";
+import TodoForm from "./components/TodoForm";
+import TodoList from "./components/TodoList";
+
+function App() {
+  const [todos, setTodos] = useState([]);
+
+  // Load from localStorage
+  useEffect(() => {
+    const savedTodos = JSON.parse(localStorage.getItem("todos"));
+    if (savedTodos) {
+      setTodos(savedTodos);
+    }
+  }, []);
+
+  // Save to localStorage
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }, [todos]);
+
+  const addTodo = (text) => {
+    const newTodo = {
+      id: Date.now(),
+      text: text,
+      completed: false,
+    };
+    setTodos([...todos, newTodo]);
+  };
+
+  const deleteTodo = (id) => {
+    setTodos(todos.filter((todo) => todo.id !== id));
+  };
+
+  const toggleComplete = (id) => {
+    setTodos(
+      todos.map((todo) =>
+        todo.id === id ? { ...todo, completed: !todo.completed } : todo
+      )
+    );
+  };
+
+  return (
+    <div className="app">
+      <h1>Todo List App</h1>
+      <TodoForm addTodo={addTodo} />
+      <TodoList
+        todos={todos}
+        deleteTodo={deleteTodo}
+        toggleComplete={toggleComplete}
+      />
+    </div>
+  );
+}
+
+export default App;
