0e3016ad014887142c33c67fc786da461490cc70
Ranjith.HL <ranjithranjith2747@gmail.com>
2026-03-01 23:21:04 +0530

    Create TodoItem.js

---
+++ src/src/src/components/src/components/src/components/TodoItem.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+function TodoItem({ todo, deleteTodo, toggleComplete }) {
+  return (
+    <div className="todo-item">
+      <span
+        onClick={() => toggleComplete(todo.id)}
+        style={{
+          textDecoration: todo.completed ? "line-through" : "none",
+          cursor: "pointer",
+        }}
+      >
+        {todo.text}
+      </span>
+      <button onClick={() => deleteTodo(todo.id)}>Delete</button>
+    </div>
+  );
+}
+
+export default TodoItem;
